ca46ffd9179a8ae1555dfe30e10484ca0045c983
Rasmus Freund <rasmus3141@gmail.com>
2022-09-12 15:04:33 +0200

    Implemented all strategies

--- src/computer-guessing.py
+++ src/computer-guessing.py
@@ -23,7 +23,38 @@ print("Let me know how good my guess is.\n")
 # Here, we implement the computer's strategy for guessing
 # the number you are thinking of. Don't lie to the
 # computer. It won't punish you, but it will frown upon it.
-for guess in range(1, 21):
+
+# Guessing low to high
+# for guess in range(1, 21):
+#     result = input_selection(
+#         "I'm guessing {}\nHow is my guess?".format(guess),
+#         ["low", "hit", "high"]
+#     )
+#     if result == "hit":
+#         print("Wuhuu!")
+#         break
+
+#     print("I must have been too low, right?", result)
+
+
+# Guessing high to low
+# for guess in range(20, 0, -1):
+#     result = input_selection(
+#         "I'm guessing {}\nHow is my guess?".format(guess),
+#         ["low", "hit", "high"]
+#     )
+#     if result == "hit":
+#         print("Wuhuu!")
+#         break
+
+#     print("I must have been too high, right?", result)
+
+# Guessing midpoint of intervals
+
+interval_low, interval_high = 0, 20
+guess = (interval_low + interval_high) // 2
+
+while True:
     result = input_selection(
         "I'm guessing {}\nHow is my guess?".format(guess),
         ["low", "hit", "high"]
@@ -31,5 +62,14 @@ for guess in range(1, 21):
     if result == "hit":
         print("Wuhuu!")
         break
+    elif result == "low":
+        print("I must have been too low, right?", result)
+        interval_low = guess
+        guess = (interval_low + interval_high) // 2
+        continue
+    elif result == "high":
+        print("I must have been too high, right?", result)
+        interval_high = guess
+        guess = (interval_low + interval_high) // 2
+        continue
 
-    print("I must have been too low, right?", result)
